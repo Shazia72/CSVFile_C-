8aa12aeba56bfafc093705c063bb00b25f119c36
Shazia Idrees <vohragee@gmail.com>
2024-01-03 22:02:54 -0500

    Json Data Is added as an object

---
+++ OrderJsonConsoleApp/CustomerDataResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderJsonConsoleApp
+{
+    public class CustomerDataResponse
+    {
+        public List<VM> CustomerData { get; set; }
+    }
+    public class VM
+    {
+        public double OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ OrderJsonConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace OrderJsonConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            apiCall(client).Wait();
+        }
+        static async Task apiCall(HttpClient client)
+        {
+            try
+            {
+                using (client) {
+                    var response = await client.GetAsync("https://www.etlbox.net/demo/api/orders");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseBody =  response.Content.ReadAsStringAsync().Result;
+                        var customers =  JsonConvert.DeserializeObject<CustomerDataResponse>(responseBody);
+
+                        foreach (var cust in customers.CustomerData)
+                        {
+                            var order = new VM();
+                            order.Description = cust.Description.Split(':')[0];
+                            order.Quantity = Convert.ToInt32( cust.Description.Split(':')[1]);
+                            order.OrderNumber = cust.OrderNumber;
+                            order.CustomerId = cust.CustomerId;
+                            Console.WriteLine(order);
+                        }
+                       
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            
+        }
+               
+    }
+}
